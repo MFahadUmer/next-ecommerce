59f0c920a6a4108c4f8799ec7239d92c3f13e30c
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2024-01-31 18:02:52 +0500

    Displays Store Name

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,7 +1,16 @@
+interface DashboardPageProps {
+  params: { storeId: string };
+}
+import prismadb from '@/lib/prismadb';
 import React from 'react';
 
-const DashboardPage = () => {
-  return <div>This is a dashboard</div>;
+const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+    },
+  });
+  return <div>Active Store: {store?.name}</div>;
 };
 
 export default DashboardPage;
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -8,14 +8,17 @@ export default async function SetupLayout({
   children: React.ReactNode;
 }) {
   const { userId } = auth();
+
   if (!userId) {
     redirect('/sign-in');
   }
+
   const store = await prismadb.store.findFirst({
     where: {
       userId,
     },
   });
+
   if (store) {
     redirect(`/${store.id}`);
   }
